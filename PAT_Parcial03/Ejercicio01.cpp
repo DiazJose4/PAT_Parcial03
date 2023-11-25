e09046a3885d8c6d30a8716a7b6cb9f2f5ff2829
JOSE CARLOS DIAZ GUERRERO <jcdiazgu@correo.url.edu.gt>
2023-11-25 14:45:47 -0600

    P4

--- PAT_Parcial03/Ejercicio01.cpp
+++ PAT_Parcial03/Ejercicio01.cpp
@@ -1,6 +1,47 @@
+#include <queue>
+#include <unordered_map>
+#include <map>
 #include "Ejercicio01.h"
+
+
+//Ordenamiento entre listas
+Node<int>* mergeSortedLists(Node<int>* list1, Node<int>* list2) {
+    Node<int> header{ 0 };
+    Node<int>* current = &header;
+
+    while (list1 != nullptr && list2 != nullptr) {
+        if (list1->value > list2->value) {
+            current->next = list1;
+            list1 = list1->next;
+        }
+        else {
+            current->next = list2;
+            list2 = list2->next;
+        }
+        current = current->next;
+    }
+
+    if (list1 != nullptr) {
+        current->next = list1;
+    }
+    else {
+        current->next = list2;
+    }
+
+    return header.next;  
+}
+
 
 Node<int>* Ejercicio01::mergeLists(Node<Node<int>*>* lists)
 {
-    return nullptr;
+    if (lists == nullptr) {
+        return nullptr;
+    }
+
+    while (lists->next != nullptr) {
+        lists->value = mergeSortedLists(lists->value, lists->next->value);
+        lists->next = lists->next->next;
+    }
+
+    return lists->value;
 }
